2925007c0e371bde502683d758f3445ef68f4993
Jujube2005 <chotika.jakchai@gmail.com>
2026-02-19 17:42:26 +0700

    init structure

---
+++ frontend/src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://ijdrnvemifppzgsffqjv.supabase.co',
+  'sb_publishable_Heh5x5XRBmC2C_QE7BOFaQ_gHDPzmGL'
+)
