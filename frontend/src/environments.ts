11b7f363a6992e445f1db62732539f3609e57d97
Jujube2005 <chotika.jakchai@gmail.com>
2026-02-23 12:30:48 +0700

    fix: environments

--- frontend/src/environments.ts
+++ frontend/src/environments.ts
@@ -1,3 +1,2 @@
-export const supabaseUrl = ''
-export const supabaseAnonKey = ''
-
+export const supabaseUrl = 'https://ijdrnvemifppzgsffqjv.supabase.co'
+export const supabaseAnonKey = 'sb_publishable_Heh5x5XRBmC2C_QE7BOFaQ_gHDPzmGL'
