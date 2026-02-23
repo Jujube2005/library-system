eb86c99cb7aa994229cd997c307696cf5270ac00
Jujube2005 <chotika.jakchai@gmail.com>
2026-02-23 23:05:17 +0700

    updated

--- frontend/src/environments.ts
+++ frontend/src/environments.ts
@@ -1,3 +1,3 @@
-export const supabaseUrl = ''
-export const supabaseAnonKey = ''
+export const supabaseUrl = 'https://ijdrnvemifppzgsffqjv.supabase.co'
+export const supabaseAnonKey = 'sb_publishable_Heh5x5XRBmC2C_QE7BOFaQ_gHDPzmGL'
 
